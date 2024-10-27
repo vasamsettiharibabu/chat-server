3187c5687f363ffd05da8af2026944f3a442fde4
hari babu <hari1234141@gmail.com>
2024-10-27 22:22:49 +0530

    master

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http, {
   cors: {
-    origins: ["http://localhost:4200/"],
+    origins: ["https://chat-client-three-peach.vercel.app/"],
   },
 });
 
